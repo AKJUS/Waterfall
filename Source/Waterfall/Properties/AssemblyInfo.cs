7970828ebb75f859324c89ccaf0108d70998634d
JonnyOThan <jonnyothan@gmail.com>
2026-04-24 14:10:41 -0400

    merge from kspbt

--- Source/Waterfall/Properties/AssemblyInfo.cs
+++ Source/Waterfall/Properties/AssemblyInfo.cs
@@ -5,11 +5,7 @@ using System.Runtime.InteropServices;
 // General Information about an assembly is controlled through the following 
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("Waterfall")]
 [assembly: AssemblyDescription("")]
-[assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("Waterfall")]
 [assembly: AssemblyCopyright("Copyright ©2024")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
@@ -22,15 +18,3 @@ using System.Runtime.InteropServices;
 // The following GUID is for the ID of the typelib if this project is exposed to COM
 [assembly: Guid("ca27a9c5-b2a2-45d8-a9ac-2053bb93c2d6")]
 
-// Version information for an assembly consists of the following four values:
-//
-//      Major Version
-//      Minor Version 
-//      Build Number
-//      Revision
-//
-// You can specify all the values or you can default the Build and Revision Numbers 
-// by using the '*' as shown below:
-// [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("0.10.2")]
-[assembly: AssemblyFileVersion("0.10.2")]
