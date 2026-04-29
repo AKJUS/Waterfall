d38bae39e96dec2801570fedb4b2304a17d43ba0
JonnyOThan <jonnyothan@gmail.com>
2026-04-29 11:46:07 -0400

    update custom pull controller editor UI

--- Source/Waterfall/UI/EffectControllersUI/CustomPullControllerUIOptions.cs
+++ Source/Waterfall/UI/EffectControllersUI/CustomPullControllerUIOptions.cs
@@ -6,7 +6,12 @@ namespace Waterfall.UI.EffectControllersUI
 {
   public sealed class CustomPullControllerUIOptions : DefaultEffectControllerUIOptions<CustomPullController>
   {
+    private string moduleTypeName;
     private string memberName;
+
+    private string engineIDFieldName;
+    private string engineID;
+    private string predicateFieldName;
 
     private string minInputValueString;
     private string maxInputValueString;
@@ -34,11 +39,31 @@ namespace Waterfall.UI.EffectControllersUI
       GUILayout.BeginVertical();
 
       GUILayout.BeginHorizontal();
+      GUILayout.Label("Module Type", UIResources.GetStyle("data_header"), GUILayout.MaxWidth(100));
+      moduleTypeName = GUILayout.TextArea(moduleTypeName, GUILayout.MaxWidth(300));
+      GUILayout.EndHorizontal();
+
+      GUILayout.BeginHorizontal();
       GUILayout.Label("Member", UIResources.GetStyle("data_header"), GUILayout.MaxWidth(100));
       memberName = GUILayout.TextArea(memberName, GUILayout.MaxWidth(300));
       GUILayout.EndHorizontal();
 
-      GUILayout.Label("Specified member should be public instance parameterless method, property or field on ModuleEngines component returning numeric value", UIResources.GetStyle("data_comment"), GUILayout.MaxWidth(600));
+      GUILayout.Label("Specified member should be parameterless method, property or field on tye module type specified above, returning numeric value", UIResources.GetStyle("data_comment"), GUILayout.MaxWidth(600));
+
+      GUILayout.BeginHorizontal();
+      GUILayout.Label("EngineID Field Name", UIResources.GetStyle("data_header"), GUILayout.MaxWidth(100));
+      engineIDFieldName = GUILayout.TextArea(engineIDFieldName, GUILayout.MaxWidth(300));
+      GUILayout.EndHorizontal();
+
+      GUILayout.BeginHorizontal();
+      GUILayout.Label("EngineID", UIResources.GetStyle("data_header"), GUILayout.MaxWidth(100));
+      engineID = GUILayout.TextArea(engineID, GUILayout.MaxWidth(300));
+      GUILayout.EndHorizontal();
+
+      GUILayout.BeginHorizontal();
+      GUILayout.Label("Predicate Field Name", UIResources.GetStyle("data_header"), GUILayout.MaxWidth(100));
+      predicateFieldName = GUILayout.TextArea(predicateFieldName, GUILayout.MaxWidth(300));
+      GUILayout.EndHorizontal();
 
       GUILayout.EndVertical();
 
@@ -65,7 +90,13 @@ namespace Waterfall.UI.EffectControllersUI
 
     protected override void LoadOptions(CustomPullController controller)
     {
+      moduleTypeName         = controller.moduleTypeName;
       memberName             = controller.memberName;
+
+      engineIDFieldName      = controller.engineIDFieldName;
+      engineID               = controller.engineID;
+      predicateFieldName     = controller.predicateFieldName;
+
       minInputValueString    = controller.minInputValue.ToString(CultureInfo.InvariantCulture);
       maxInputValueString    = controller.maxInputValue.ToString(CultureInfo.InvariantCulture);
       responseRateUpString   = controller.responseRateUp.ToString(CultureInfo.InvariantCulture);
@@ -75,7 +106,11 @@ namespace Waterfall.UI.EffectControllersUI
     protected override CustomPullController CreateControllerInternal() =>
       new()
       {
+        moduleTypeName   = moduleTypeName,
         memberName       = memberName,
+        engineIDFieldName = engineIDFieldName,
+        engineID = engineID,
+        predicateFieldName = predicateFieldName,
         minInputValue    = ParseOrZero(minInputValueString),
         maxInputValue    = ParseOrZero(maxInputValueString),
         responseRateUp   = ParseOrZero(responseRateUpString),
